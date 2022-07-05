4891c2d0e048beef08fe8a11852abae1b58b0337
VijayMalav <vijaymalav5455@gmail.com>
2022-07-05 15:48:21 +0530

    add start script for heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path =require('path');
 const hbs=require('hbs');
 const app=express();
 
-const port=process.env.PORT || 3000;
+const port= process.env.PORT || 3000;
 // public static path
 const static_path=path.join(__dirname,"../public");
 const template_path=path.join(__dirname,"../templates/views");
